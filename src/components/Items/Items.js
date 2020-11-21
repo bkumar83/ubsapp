ba16b0229acad16d5634d74bd399c7b23ed35706
bkumar83 <67649491+bkumar83@users.noreply.github.com>
2020-11-21 22:13:15 +0800

    Update Items.js

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,89 +1,119 @@
 import React from 'react';
+import './items.css';
+import { tableColumns } from '../../constants';
 import { connect } from 'react-redux';
-import { setCurrentPath, storeSelItem } from '../../actions';
-import { apiurl } from '../../constants';
-import './itemslist.css';
- 
-/**
-* @desc This component is the second page of application which gets the list of Items from public api.
-* @param setCurrentPath : Calls this method defined in action to set current path
-*/
 
-class ItemsList extends React.Component {
+/**
+ * Desc:This is the Items component which is displays the selected items
+ * @Param:item
+ */
+class Items extends React.Component {
   constructor(props) {
     super(props);
+    let category = '';
+    if (typeof this.props.selItem !== 'undefined') {
+      category = this.props.selItem.Category;
+    } else {
+      category = this.props.location.state.item.Category;
+    }
     this.state = {
-      itemsList: [],
-      loading: true
+      category: category,
+      itemDetails: [],
+      tableColumns: tableColumns,
+      isLoading: true,
     };
+
   }
   /**
-   * @desc: Gets all the items from the public api
+   * Desc:this method renders to previous page
+   */
+  onIndex = () => {
+    this.props.history.push({
+      pathname: "/"
+    });
+  }
+  /**
+   * Desc:Fetchs the list of items fof the selected category
+   * @Param:category
    */
   async componentDidMount() {
-    this.getAllItems();
-     
+
+    this.getAllItemsByCategory();
   }
 
-  async getAllItems() {
+  async getAllItemsByCategory() {
     try {
-      const response = await fetch(apiurl.ENTRIESAPI);
+      const response = await fetch(`https://api.publicapis.org/entries?category=${this.state.category}&https=true`);
       if (response.status === 200) {
         const allItems = await response.json();
-        this.setState({ itemsList: allItems.entries, loading: false });
-      } else if (response.status === 404) {
-        alert('Sorry,this page isn not  available');
+        this.setState({ itemDetails: allItems.entries, isLoading: false });
+      } else if (response.status === 400) {
+        alert('Sorry,this page is not  available');
       }
-
     } catch (error) {
+      this.setState({ isLoading: false });
       console.log(error);
-      alert('Something wrong. Please try again');
     }
   }
   /**
-   * @Desc:This method sets the current item is selected and renders to the selected item compnent
+   * Desc:this method renders to table header and sets the header colums
+   * @Param:tablecolumns
+   */
+  renderTableHeader = () => {
+    return this.state.tableColumns
+      .map((column, i) => <th key={i}>{column.title}</th>)
+  }
+  /**
+   * Desc:this method renders the tables rows and display the data
    * @Param:item
    */
-  assignItem = item => { // bound arrow function handler
-    this.props.onSelItem(item);
-    this.props.history.push({
-      pathname: "/selItem/",
-      state: { item: item }
-    });
+  renderTableRows = () => {
+    return this.state.itemDetails.map(item => {
+      return (
+        <tr key={item.Link}>
+          <td><a href={item.Link}> {item.API}</a> </td>
+          <td>{item.Description}</td>
+          <td>{item.Auth}</td>
+          <td>{item.HTTPS}</td>
+          <td>{item.Cors}</td>
+
+        </tr>
+      )
+    })
   }
 
   render() {
+    const items = this.state.itemDetails;
 
-    return (
+    return items.length > 0
+      ? (
 
-      <div class="itemContainer">
-        <div class="title">UBS APP </div>
-        <div class="index">Index</div>
-        <div class="idxline"></div>
-        {this.state.loading ? <div>Loading...</div> :
-          (<div>{this.state.itemsList.map(item => (
-            <ul>  <li key={item} onClick={e => this.assignItem(item)}>
-              <a >{item.API} </a>
-            </li></ul>
-          ))}</div>)
-        }
-
-      </div>);
+        [<div calss="maintable">
+          <div class="category">{this.state.category}</div>
+          <table class="itemtable">
+            <thead>
+              <tr>
+                {this.renderTableHeader()}
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderTableRows()}
+            </tbody>
+          </table>
+          <div calss="indexback">  <a onClick={this.onIndex.bind(this)}><g-emoji class="g-emoji" alias="arrow_up" >⬆</g-emoji>Back to Index</a> </div>
+        </div>
+        ]
+      ) : (
+        <div>{this.state.isLoading ? <div>Loading...</div> :
+          (<div> No Data{this.props.selItem}</div>)
+        }</div>
+      );
   }
 }
-
 const mapStateToProps = (state) => {
   return {
-    selItem: state.selItem
+    selItem: state.selitem
   }
-};
-
-const mapDispachToProps = (dispatch) => {
-  return {
-    currentpath: pathValue => (dispatch(setCurrentPath(pathValue))),
-    onSelItem: item => (dispatch(storeSelItem(item))),
-
-  }
-};
-export default connect(mapStateToProps, mapDispachToProps)(ItemsList);
+}
+export default connect(mapStateToProps)(Items);
 
