746419da825ba1e82dc8cd1084065de3c9456920
bkumar83 <67649491+bkumar83@users.noreply.github.com>
2020-11-22 15:05:25 +0800

    Update App.test.js

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,8 +1,62 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import * as actions from '../../actions'
+import reducer from '../../reducers'
+
+describe('actions', () => {
+    it('should create an action to add a todo', () => {
+      const pathValue = '/'
+      const expectedAction = {
+        type: 'SETCURRENTPATH',
+        payload: {
+            currentPath: '/'
+        }
+      }
+      expect(actions.setCurrentPath(pathValue)).toEqual(expectedAction)
+    })
+  })
+
+  describe('actions', () => {
+    it('should create an action to add a todo', () => {
+      const item = '';
+      const expectedAction = {
+        type: 'STOREITEM',
+        payload: {
+            selItem: ''
+        }
+      }
+      expect(actions.storeSelItem(item)).toEqual(expectedAction)
+    })
+  })
+
+
+  
+  describe('ItemList reducer', () => {
+    it('should return the initial state', () => {
+      expect(reducer(undefined, {})).toEqual(
+        {
+            currentpath: '/',
+            selItem:null
+        }
+      )
+    })
+ 
+
+    it('returns the correct state path', () => {
+        const action = { type:'SETCURRENTPATH', payload: {
+            currentpath: '/'
+        } };
+        const expectedState = { currentpath: '/', selItem:null};
+    
+        expect(reducer(undefined, action)).toEqual(expectedState)
+      })
+
+      it('returns the correct state item', () => {
+        const action = { type:'STOREITEM', payload: {
+            selItem: 'Item Details',
+            currentpat: '/'
+        } };
+        const expectedState = { currentpath: '/', selItem:'Item Details'};
+    
+        expect(reducer(undefined, action)).toEqual(expectedState)
+      })
+})
